c326c923b8dce5595a409efffd7641120bd32a0b
brendongama <brendon_gama@hotmail.com>
2021-11-12 10:25:21 -0300

    Configuring hr-user project

--- hr-api-gateway-zuul/src/main/java/com/brendon/hrapigatewayazuul/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/com/brendon/hrapigatewayazuul/HrApiGatewayZuulApplication.java
@@ -1,4 +1,4 @@
-package com.brendon.hreurekaserver;
+package com.brendon.hrapigatewayazuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
