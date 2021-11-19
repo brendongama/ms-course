e27e10de47bda4ead327ee5c7836205607753812
brendongama <brendon_gama@hotmail.com>
2021-11-19 10:59:44 -0300

    Login, JWT token

--- hr-oauth/src/main/java/com/brendon/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/brendon/hroauth/services/UserService.java
@@ -1,13 +1,16 @@
 package com.brendon.hroauth.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.brendon.hroauth.entities.User;
 import com.brendon.hroauth.feignClients.UserFeignClients;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService{
 	
 	@Autowired
 	private UserFeignClients userFeignClients;
@@ -19,6 +22,16 @@ public class UserService {
 			throw new IllegalArgumentException("Email not found");
 		}
 		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClients
+				.findByEmail(username).getBody();
+				if(user == null ) {
+					throw new UsernameNotFoundException("Email not found");
+				}
+				return user;
 	}
 	
 }
